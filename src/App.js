fbae91de5da1812a64af17cd264e17f897d6ef6c
Edwin Webb <edwin.webb@gmail.com>
2022-09-17 14:57:19 -0700

    V1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
-import { Canvas } from '@react-three/fiber'
+import { Box, OrbitControls } from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber'
+import { CylinderCollider, Debug, Physics, RigidBody } from '@react-three/rapier';
+import { Controllers, Hands, useController, useInteraction, VRButton, XR, XRButton } from '@react-three/xr';
+import { useState, useRef, useEffect } from 'react';
+
+// board
+// chips
+const Chips = () => {
+  const [chips, setChips] = useState([1,2,3,4,5]);
+  return(
+    <>
+     {chips.map((chip, index) => (
+        <Chip key={index} />
+      ))}
+    </>
+  )
+}
+
+const Chip = () => {
+  const color = 'red'
+
+  return (
+    <RigidBody colliders={false} position={[0, 5, 0]}>
+      <mesh>
+        <cylinderBufferGeometry args={[0.04, 0.04, 0.01]} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+      <CylinderCollider args={[0.005, 0.04]} />
+    </RigidBody>
+  );
+};
+
+const Floor = () => {
+  return (
+    <RigidBody colliders="cuboid" type="fixed">
+      <Box args={[10, 0.1, 10]}>
+        <meshStandardMaterial color="green" />
+      </Box>
+    </RigidBody>
+  );
+};
 
 function App() {
-  return (
+  return (<>
     <Canvas>
-      <mesh position={[0,0,-5]} rotation={[0, 0, 0]}>
-        <torusGeometry args={[4, 0.5, 16, 32]} />
-        <meshBasicMaterial wireframe color="green" />
-      </mesh>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <XR>
+        <Physics>
+          <Debug color="red" sleepColor="blue" />
+          <Chip />
+          <Floor />
+        </Physics>
+      </XR>
+      <OrbitControls />
     </Canvas>
-  );
+    <VRButton />
+  </>);
 }
 
 export default App;
